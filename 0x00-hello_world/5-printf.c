4aa0558693d063094e8ea95fa74d923126cac979
Abir S <you@example.com>
2022-09-14 10:02:05 -0700

    hj

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,9 +10,9 @@ int main(void)
 	 *
 	 *     * Return: Always 0.
 	 *
-	 *      */
+	 */
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
