e42a34db7576073622e8e7b53ffe2ce77007bd21
Abir S <you@example.com>
2022-09-14 07:08:48 -0700

    lk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
+/*
+ * file :4-puts.c
+ * autore:Abir .S
+ */
 #include <stdio.h>
-int main(void)
+/*
+ * main :prints "Programming is like building a multilingual
+ *
+ *  *          puzzle folowed by new line
+ *  return 0
+ */
+int main (void);
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 return (0);
+
 }
