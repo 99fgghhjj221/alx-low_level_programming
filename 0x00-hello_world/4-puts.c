ba31f77dbbd79ac54ca06164e232ed52b591afa1
Abir S <you@example.com>
2022-09-14 07:25:45 -0700

    df

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,14 @@
 #include <stdio.h>
 /*
  * main :prints "Programming is like building a multilingual
+ *                puzzle folowed by new line
  *
- *  *          puzzle folowed by new line
- *  return 0
+ *  return : Always zero
  */
 int main(void)
+	/*
+	 * main is a function
+	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
