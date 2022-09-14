da67dee85a3bb0930bed680f36086f1d7f74e26d
Abir S <you@example.com>
2022-09-14 08:13:05 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main(void)
+int main(void)/**
+		*main print the
+                *thc below string
+		*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
