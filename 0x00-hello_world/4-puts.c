93d59b8c59e24724214a869ef6f905f7297b95b4
Abir S <you@example.com>
2022-09-14 08:07:10 -0700

    df

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,6 @@
-/*
- *File :4-puts.c
- * autore:Abir .S
- */
-
 #include <stdio.h>
-/*
- * main :prints "Programming is like building a multilingual
- *                puzzle folowed by new line
- *
- *  return : Always zero
- */
-int main(void)/*THIS IS MAIN FUNCTION*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-       return (0);
-
+	return (0);
 }
