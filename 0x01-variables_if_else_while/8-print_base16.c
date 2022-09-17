f3e0cf4d79dcb76631f09f83e541aef8c612171f
Abir S <you@example.com>
2022-09-17 16:19:21 -0700

    ff

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+/*
+ * File: 8-print_base16.c
+ * Auth: t prints all the numbers of base 16 in lowercase
+ *
+ */
+#include <stdio.h>
+
+/**
+ * main - t prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+
+	char hex = 'a';
+
+	for (; i < 10; i++)
+	{
+		putchar(i + '0');
+		i = i % 10;
+	}
+	for (; hex < 'f'; hex++)
+		putchar(hex);
+	
+	putchar('\n');
+
+	return (0);
+}
