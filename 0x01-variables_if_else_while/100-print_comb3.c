d1a301c11e768e853b958f1b8e614a8483d72a18
Abir S <you@example.com>
2022-09-18 02:57:04 -0700

    gg

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Abir S
+ */
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i1, i2;
+
+	for (i1 = 0; i1 <9; i1++)
+	{
+		for (i2 = i1 + 1; i2 < 10; i2++)
+		{		
+			putchar((i1 % 10) + '0');
+			putchar((i2 % 10) + '0');
+
+			if (i1 == 8 && i2 == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+	
+		}
+	}
+putchar('\n');
+
+return (0);
+}
