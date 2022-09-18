4dec6afbde931c67cb1373f2a6c268f4660f556c
Abir S <you@example.com>
2022-09-17 17:27:41 -0700

    gh

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,15 @@ int main(void)
 {
 	int i = 0;
 
-	for(; i < 10; i++)
+	for (; i < 10; i++)
 	{
 		putchar(i + '0');
 		i = i % 10;
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 putchar('\n');
 
