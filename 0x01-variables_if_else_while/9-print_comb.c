3682dd502aef7c7f17e4ef48f79ead6f4847e6e2
Abir S <you@example.com>
2022-09-17 17:13:18 -0700

    gh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: abir s
+ */
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+
+	for(; i < 10; i++)
+	{
+		putchar(i + '0');
+		i = i % 10;
+		putchar(',');
+		putchar(' ');
+	}
+putchar('\n');
+
+return (0);
+}
