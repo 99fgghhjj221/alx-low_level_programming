85a6ee9018f2e512c165ac6f1b41dd6bfa2f5bcc
Abir S <you@example.com>
2022-09-18 03:01:39 -0700

    jk

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,10 +13,10 @@ int main(void)
 {
 	int i1, i2;
 
-	for (i1 = 0; i1 <9; i1++)
+	for (i1 = 0; i1 < 9; i1++)
 	{
 		for (i2 = i1 + 1; i2 < 10; i2++)
-		{		
+		{
 			putchar((i1 % 10) + '0');
 			putchar((i2 % 10) + '0');
 
@@ -26,7 +26,6 @@ int main(void)
 			}
 			putchar(',');
 			putchar(' ');
-	
 		}
 	}
 putchar('\n');
