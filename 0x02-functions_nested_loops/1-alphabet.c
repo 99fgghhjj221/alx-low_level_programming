19bb5b4acd37ca620ddc95cc3c1d1e79c95d662f
Abir S <you@example.com>
2022-09-20 03:32:24 -0700

    gh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,22 +3,21 @@
  * Auth: Abir S
  *
  */
-#include <_putchar.h>
+
+#include "_putchar.h"
 
 /**
  * main - prints the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar('letter');
+		_putchar(letter);
 	}
 	_putchar('\n');
-
-return (0);
 }
