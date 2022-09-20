4246b1f7c4bbc53329bb1fa33f5578030a2649ba
Abir S <you@example.com>
2022-09-20 09:39:15 -0700

    fg

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+/*
+ * File: 3-islower.c
+ * Auth: Abir S
+ */
+#include "main.h"
+
+/**
+ * _islower - PRINTS THE LOWER ONE
+ * @c: char to be checked
+ *
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+return (0);
+}
