319f4867e61e74a49399e90fbe8c340dd883479b
Abir S <you@example.com>
2022-09-20 12:17:14 -0700

    df

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Abir S
+ */
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: char to be cheaked
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+return(0);
+}
