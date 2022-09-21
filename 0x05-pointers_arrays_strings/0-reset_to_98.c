0d850b9738154a27dc3d5e9c848114f833cc3b9f
Abir S <you@example.com>
2022-09-21 00:01:03 -0700

    awosome

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Abir .S
+ */
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the values of a pointer
+ * @n: value to be reseted
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
